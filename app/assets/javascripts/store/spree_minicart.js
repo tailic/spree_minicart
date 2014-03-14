228c6ac8a78431b0e7ae8ab94e2df1151bb4e686
tailic <niklas@derschreiber.com>
2014-03-14 12:47:06 +0100

    refactoring js

--- app/assets/javascripts/store/spree_minicart.js
+++ app/assets/javascripts/store/spree_minicart.js
@@ -9,43 +9,32 @@
                 linkToCart:     "#link-to-cart",
                 miniCart:       "#minicart",
                 progress:       "#progress",
-                form:           "form#update-minicart"
+                form:           "form#update-minicart",
+                formDelete:     this.form + " a.delete"
             },
             hoverIntent: {
                 over: function () {
-                    $(config.selectors.miniCart).slideDown();
+                    $(config.selectors.miniCart).show();
                 },
                 timeout: 250, // number = milliseconds delay before onMouseOut
                 out: function () {
-                    $(config.selectors.miniCart).slideUp();
+                    $(config.selectors.miniCart).hide();
                 }
             }
+        }
+
+        var showMiniCart = function () {
+            $(config.selectors.miniCart).show();
+        };
+
+        var hideMiniCart = function () {
+            $(config.selectors.miniCart).hide();
         };
 
 
         $(config.selectors.linkToCart).hoverIntent(config.hoverIntent);
 
-        $(document).on('mouseover', "ul#minicart-items li", function (e) {
-            var $this = $(this);
-            if (!$this.data('init')) {
-                $this.data('init', true);
-                $this.hoverIntent({
-                    over: function () {
-                        $(this).find("[data-hook='minicart_item_description']").hide();
-                        $(this).find("[data-hook='minicart_item_actions']").show();
-                    },
-                    timeout: 100, // number = milliseconds delay before onMouseOut
-                    out: function () {
-                        $(this).find("[data-hook='minicart_item_description']").show();
-                        $(this).find("[data-hook='minicart_item_actions']").hide();
-                    }
-                });
-                $this.trigger(e);
-            }
-        });
-
-
-        $(document).on('click', config.selectors.form + 'a.delete', function (e) {
+        $(document).on('click', config.selectors.form + ' a.delete', function (e) {
             var $this = $(this);
             $this.parent().siblings('div[data-hook="minicart_item_quantity"]').find("input.line_item_quantity").val(0);
             $this.parents('form').first().submit();
@@ -55,24 +44,24 @@
         //TODO make ajax progress indication more user friendly
         //TODO #update-minicart to scope the form selector? there can be more remote forms on page!?
         $(document).on("ajax:beforeSend", "form[data-remote]", function () {
-            $(config.progress).slideDown();
+            $(config.progress).show();
         });
 
         $(document).on("ajax:complete", "form[data-remote]", function () {
-            $(config.progress).slideUp();
+            $(config.progress).hide();
         });
 
-        $(document).on('mouseenter', config.selectors.miniCart, function (e) {
-            $(config.selectors.miniCart).show();
+        $(document).on('mouseenter', config.selectors.miniCart, function () {
+            showMiniCart();
         });
 
-        $(document).on('mouseleave', config.selectors.miniCart, function (e) {
-            $(config.selectors.miniCart).slideUp();
+        $(document).on('mouseleave', config.selectors.miniCart, function () {
+            hideMiniCart();
         });
 
         //TODO this will fire on all clicks... bind only if minicart is open and unbind after
-        $(document).on('click', "body", function (e) {
-            $(config.selectors.miniCart).slideUp();
+        $(document).on('click', "body", function () {
+            hideMiniCart();
         });
     });
 })(jQuery);
