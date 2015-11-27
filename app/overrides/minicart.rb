ece6881cab0c6aaae67c79373304f304c3272e61
Niklas Schreiber <niklas@derschreiber.com>
2015-11-27 14:06:54 +0100

    typo

--- app/overrides/minicart.rb
+++ app/overrides/minicart.rb
@@ -2,4 +2,4 @@ Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
                     :name => 'add_mini_cart',
                     :insert_bottom => "#link-to-cart",
                     :partial => "spree/shared/minicart",
-                    :disabled => true))
+                    :disabled => true)
