9a584de80818de262cfa10560b3747d283026e38
Niklas Schreiber <niklas@derschreiber.com>
2015-11-27 14:06:41 +0100

    disable overrides

--- app/overrides/layout.rb
+++ app/overrides/layout.rb
@@ -2,4 +2,5 @@
 Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                     :name => 'append_loading_message',
                     :insert_top => "#wrapper",
-                    :text => %q{<div id="progress"><%= image_tag 'admin/progress.gif' %> <%= t(:loading) %>...</div>})
+                    :text => %q{<div id="progress"><%= image_tag 'admin/progress.gif' %> <%= t(:loading) %>...</div>},
+                    :disabled => true)
--- app/overrides/minicart.rb
+++ app/overrides/minicart.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
                     :name => 'add_mini_cart',
                     :insert_bottom => "#link-to-cart",
-                    :partial => "spree/shared/minicart")
+                    :partial => "spree/shared/minicart",
+                    :disabled => true))
